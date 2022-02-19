402f4cd79260979691f70a562836978e1fcb2dbe
ANUPAM KUMAR <aanupamraj@gmail.com>
2022-02-19 21:50:49 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BindingComponent } from './binding/binding.component';
+import { CareerComponent } from './career/career.component';
+import { ClientComponent } from './client/client.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home' , component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'career', component:CareerComponent},
+  {path:'client',  component:ClientComponent},
+  {path:'product' , component:ProductComponent},
+  {path:'binding', component:BindingComponent},
+  {path:'login' , component: LoginComponent},
+  {path:'signup' , component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BindingComponent } from './binding/binding.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CareerComponent } from './career/career.component';
+import { ProductComponent } from './product/product.component';
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindingComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    AboutComponent,
+    CareerComponent,
+    ProductComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
